60ebecf541bf2af1a68a16c96104ec9f7af14388
Jiei-Dux <Jiei_Dux@proton.me>
2023-09-19 21:30:32 +0800

    Activities

---
+++ 02_Lab_Exercise/ComputeAverageApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ComputeAverageApp {
+
+  class ComputeAverageProgram {
+
+    static void Main(String[] args) {
+
+      Console.WriteLine("Enter five(5) grades separated by a new line: ");
+      double Gr1 = Convert.ToDouble(Console.ReadLine());
+      double Gr2 = Convert.ToDouble(Console.ReadLine());
+      double Gr3 = Convert.ToDouble(Console.ReadLine());
+      double Gr4 = Convert.ToDouble(Console.ReadLine());
+      double Gr5 = Convert.ToDouble(Console.ReadLine());
+
+      double added = Gr1 + Gr2 + Gr3 + Gr4 + Gr5;
+      double avg = added / 5;
+
+      int RoundOff = Convert.ToInt32(Math.Round(avg));
+
+      Console.WriteLine("The average is " + avg + " and round off to " + RoundOff );
+      Console.Write("Press any key to exit...");
+      Console.ReadKey();
+
+    }
+
+  }
+
+}
---
+++ 03_Lab_Exercise/03_Lab_Exer_1.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Windows.Forms;
+
+public class Program {
+
+  [STAThread]
+  public static void Main() {
+
+    var f = new Form();
+    f.Text = "Hello World";
+    Application.Run(f);
+
+  }
+
+}
