2e6406a66d6f4909a942030d814d71399e4cf1fe
Jiei-Dux <Jiei_Dux@proton.me>
2023-09-21 09:36:56 +0800

    moved 01 and 02 inside Prelim folder

---
+++ 01 Prelim/02_Lab_Exercise/ComputeAverageApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ComputeAverageApp {
+
+  class ComputeAverageProgram {
+
+    static void Main(String[] args) {
+
+      Console.WriteLine("Enter five(5) grades separated by a new line: ");
+      
+      double[] Grades = new double[5];
+
+      int arrayNum = 0;
+
+      do {
+        Grades[arrayNum] = Convert.ToDouble(Console.ReadLine());
+        arrayNum++;
+      } while (arrayNum < Grades.Length);
+
+      double GradeAdd = 0;
+      foreach (double usrInput in Grades) {
+        GradeAdd += usrInput;
+      }
+
+      double Avg = GradeAdd / Grades.Length;
+      int RoundOff = Convert.ToInt32(Math.Round(Avg));
+
+      Console.WriteLine("The average is " + Avg + " and got rounded off to " + RoundOff);
+
+      Console.WriteLine("Press ANY key to exit...");
+      Console.ReadKey();
+
+    }
+
+  }
+
+}
