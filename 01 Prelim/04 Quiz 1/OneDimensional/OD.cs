26adb07d58fe139411443d64a08850efc364cff9
Jiei-Dux <Jiei_Dux@proton.me>
2023-09-27 21:46:05 +0800

    New Activities and Task Performance

---
+++ 01 Prelim/04 Quiz 1/OneDimensional/OD.cs
@@ -0,0 +1,24 @@
+﻿/*
+ *  C# Console Application
+ *  04 Quiz 1 - OneDimensional.cs
+ *  Computer Programming 4
+ */
+
+using System;
+
+namespace OneDimension
+{
+  class OneDimensionProgram
+  {
+    static void Main(String[] args)
+    {
+      string[] Names = { "Adriel", "Alex", "Carmen", "Jay", "Jeremiah", "Rose" };
+
+      foreach ( string ListNames in Names ) 
+      {
+        Console.WriteLine(ListNames);
+      }
+    }
+  }
+}
+
